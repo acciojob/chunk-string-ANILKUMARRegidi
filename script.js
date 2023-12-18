1eade099200e3d450a0f9cceedffb6a149a99cd5
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-18 15:42:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-function stringChop(str, size) {
-  // your code here
+ function chunkString(str, size) {
+  const res = [];
+  if (!str) {
+    return res; // Return an empty array if the input string is null
+  }
+
+  size = parseInt(size);
+   let chunk = "";
+  for (let i = 0; i < str.length; i++) {
+	  chunk += str[i];
+    if(chunk.length === size)
+	{
+    res.push(chunk);
+	chunk = str[i];
+	}
+	  else
+	{
+		res.push(str[i]);
+	}
+  }
+
+  return res;
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+//const str = prompt("Enter String.");
+//const size = prompt("Enter Chunk Size.");
+alert(chunkString(str, size));
