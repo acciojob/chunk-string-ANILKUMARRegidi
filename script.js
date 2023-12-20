5dcb42fda7290247ac9c8943b4e4ed1e5a7921cf
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-20 10:51:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
- function chunkString(str, size) {
+function chunkString(str, size) {
   const res = [];
-  if (!str) {
-    return res; // Return an empty array if the input string is null
+  if (!str || isNaN(size) || size <= 0) {
+    return res; // Return an empty array if the input is invalid
   }
 
   size = parseInt(size);
-   let chunk = "";
+  let chunk = "";
   for (let i = 0; i < str.length; i++) {
-	  chunk += str[i];
-    if(chunk.length === size)
-	{
-    res.push(chunk);
-	chunk = str[i];
-	}
-	  else
-	{
-		res.push(str[i]);
-	}
+    chunk += str[i];
+    if (chunk.length === size || i === str.length - 1) {
+      res.push(chunk);
+      chunk = ""; // Reset the chunk for the next iteratio
+    }
   }
 
   return res;
 }
 
-// Do not change the code below
-//const str = prompt("Enter String.");
-//const size = prompt("Enter Chunk Size.");
+// Example of usage
+const str = prompt("Enter String.");
+const size = prompt("Enter Chunk Size.");
 alert(chunkString(str, size));
